1591a0f03ea8e31d347b85c9952f972312ec0441
OmBedre <osbedre2003@gmail.com>
2024-08-06 00:49:53 +0530

    Student model creation

--- StudentSphere/StudentSphere/HOD_Views.py
+++ StudentSphere/StudentSphere/HOD_Views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import redirect,render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+
+from app.models import Course, Session_Year
 
 
 @login_required(login_url='/')
 def home(request):
-    return render(request,'HOD/home.html')
+    return render(request, 'HOD/home.html')
 
 
 def VIEW_STUDENTS(request):
-    return render(request,'HOD/view_student.html')
+    return render(request, 'HOD/view_student.html')
 
 
 def ADD_STUDENTS(request):
-    return render(request,'HOD/add_student.html')
+    course = Course.objects.all()
+    session_year = Session_Year.objects.all()
+
+    context = {
+        'course': course,
+        'session_year': session_year
+    }
+    return render(request, 'HOD/add_student.html', context)
